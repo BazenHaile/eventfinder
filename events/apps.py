90aab9b879b92b70a76903f59d0741d98e2b50b1
BazenHaile <113728471+BazenHaile@users.noreply.github.com>
2024-06-26 19:13:33 +0100

    Update apps.py

--- events/apps.py
+++ events/apps.py
@@ -1,9 +1,14 @@
+
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+def create_event_permissions(sender, **kwargs):
+    from .permissions import create_event_permissions
+    create_event_permissions()
 
 class EventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
 
     def ready(self):
-        from .permissions import create_event_permissions
-        create_event_permissions()
+        post_migrate.connect(create_event_permissions, sender=self)
